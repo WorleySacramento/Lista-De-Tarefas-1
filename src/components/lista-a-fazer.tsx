f4ac881c34c4379066bcdf9266de57db7b8e327a
Worley Sacramento <worleymartinsns@gmail.com>
2023-05-03 13:22:19 -0300

    update

---
+++ src/components/lista-a-fazer.tsx
@@ -0,0 +1,14 @@
+import {Box, Card, CardHeader, } from '@mui/material'
+
+
+export default function Lista_a_fazer() {
+
+    <Box>
+        <Card>
+            <CardHeader>
+                <Card.Title>A Fazer</Card.Title>
+            </CardHeader>
+        </Card>
+    </Box>
+    
+};
